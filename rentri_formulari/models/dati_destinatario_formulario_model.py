ab0120edec4fbc273084835489c0105c7a6d410e
Diego <ddelcorto@teuron.it>
2026-06-16 16:57:06 +0200

    fix validazione modello

--- rentri_formulari/models/dati_destinatario_formulario_model.py
+++ rentri_formulari/models/dati_destinatario_formulario_model.py
@@ -102,9 +102,13 @@ class DatiDestinatarioFormularioModel(BaseModel):
             return DatiDestinatarioFormularioModel.parse_obj(obj)
 
         _obj = DatiDestinatarioFormularioModel.parse_obj({
-            "autorizzazione": AutorizzazioneModel.from_dict(obj.get("autorizzazione")) if obj.get("autorizzazione") is not None else None,
+            "autorizzazione": AutorizzazioneModel.from_dict(obj["autorizzazione"]) if obj.get("autorizzazione") is not None else None,
             "attivita": obj.get("attivita"),
-            "numero_iscrizione_albo": obj.get("numero_iscrizione_albo")
+            "numero_iscrizione_albo": obj.get("numero_iscrizione_albo"),
+            "denominazione": obj.get("denominazione"),
+            "codice_fiscale": obj.get("codice_fiscale"),
+            "nazione_id": obj.get("nazione_id"),
+            "indirizzo": IndirizzoModel.from_dict(obj["indirizzo"]) if obj.get("indirizzo") is not None else None
         })
         return _obj
 
