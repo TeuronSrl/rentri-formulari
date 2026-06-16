48bb83117196d2775cf27a18ddfa9bb8f76314f5
Diego <ddelcorto@teuron.it>
2026-06-16 15:00:51 +0200

    fix validazione modello

--- rentri_formulari/models/dati_produttore_formulario_sito_model.py
+++ rentri_formulari/models/dati_produttore_formulario_sito_model.py
@@ -117,7 +117,8 @@ class DatiProduttoreFormularioSitoModel(BaseModel):
             "luogo_produzione": IndirizzoModel.from_dict(obj.get("luogo_produzione")) if obj.get("luogo_produzione") is not None else None,
             "autorizzazione": AutorizzazioneModel.from_dict(obj.get("autorizzazione")) if obj.get("autorizzazione") is not None else None,
             "detentore": obj.get("detentore"),
-            "numero_iscrizione_albo": obj.get("numero_iscrizione_albo")
+            "numero_iscrizione_albo": obj.get("numero_iscrizione_albo"),
+            "num_iscr_sito": obj.get("num_iscr_sito")
         })
         return _obj
 
